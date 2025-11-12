ecd4980f6b0eba2328432717de831c5a705a9d74
SkyPromp <poppe1max@gmail.com>
2025-11-12 10:19:56 +0100

    click female

--- cypress/e2e/htf2025.cy.js
+++ cypress/e2e/htf2025.cy.js
@@ -2,5 +2,7 @@ describe("template spec", () => {
   it("passes", () => {
     cy.visit("https://hackthefuture.bignited.be");
     cy.get("button[class^='center-button']").click();
+    cy.wait(10000);
+    cy.get("div[id='female']").click();
   });
 })
