d77e54249c6d4cf44a398424a42ce8981d5d5205
Robin Breugelmans <s148461@ap.be>
2025-11-12 13:39:39 +0100

    fix

--- cypress/e2e/level7.js
+++ cypress/e2e/level7.js
@@ -5,12 +5,12 @@ export function level7() {
     let letter = cy.get(`div[class='draggable-cube', data-letter='${word[i]}']`);
     let target = cy.get(`div[class='target-slot', data-letter='${word[i]}']`);
     
-    const dataTransfer = new DataTransfer;
     if (!Array.isArray(letter)) {
       letter = [letter];
       target = [target];
     }
-
+    
+    const dataTransfer = new DataTransfer;
     for (let j = 0; j < letter.length;) {
       letter[j].trigger('dragstart', { dataTransfer });
       target[j].trigger('drop', { dataTransfer });
