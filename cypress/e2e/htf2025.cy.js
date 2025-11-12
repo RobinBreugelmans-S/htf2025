fb3a762c85f8729a049eb4dcb097be7d0a20fd1c
Robin Breugelmans <s148461@ap.be>
2025-11-12 10:49:16 +0100

    javacsript

--- cypress/e2e/htf2025.cy.js
+++ cypress/e2e/htf2025.cy.js
@@ -1,14 +1,23 @@
 import { disableMotion } from "./util";
 
 Cypress.on('window:before:load', (cyWindow) => {
-  disableMotion(cyWindow);
+  //disableMotion(cyWindow);
 });
 
 describe("template spec", () => {
+  before(() => {
+    cy.clock();
+  })
+
   it("passes", () => {
     cy.visit("https://hackthefuture.bignited.be");
     cy.get("button[class^='center-button']").click();
+    cy.tick(25000);
     cy.get("div[id='female']").click();
-    cy.get("button").should('contain', 'Yes').click();
+    cy.tick(1000);
+    cy.get("button[class='btn btn-primary']").click();
+    cy.tick(200000);
+
+    cy.get
   });
 })
