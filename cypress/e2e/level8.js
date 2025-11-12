7ee1e88182ebd86ff7d8d7661682f4fae519c878
Robin Breugelmans <s148461@ap.be>
2025-11-12 15:48:17 +0100

    dev -> div

--- cypress/e2e/level8.js
+++ cypress/e2e/level8.js
@@ -1,11 +1,8 @@
 export function level8() {
-  cy.get("dev[id='crystal']").click();
+  cy.get("div[id='crystal']").click();
   const crystalButton = cy.get("button[class='crystal-outside']")
   crystalButton.click();
   crystalButton.trigger("mousedown"); 
-  /*, {
-  button: 0,
-  } */
   cy.tick(10000);
   crystalButton.trigger("mouseup");
 }
