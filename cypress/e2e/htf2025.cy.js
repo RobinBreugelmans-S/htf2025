f9c8211df3328c14f0c6c04c7e66a30e34f3c0a5
Robin Breugelmans <s148461@ap.be>
2025-11-12 10:29:41 +0100

    add enable and disable animations

--- cypress/e2e/htf2025.cy.js
+++ cypress/e2e/htf2025.cy.js
@@ -1,10 +1,14 @@
+import { disableMotion } from "./util";
+
+Cypress.on('window:before:load', (cyWindow) => {
+  disableMotion(cyWindow);
+});
+
 describe("template spec", () => {
   it("passes", () => {
     cy.visit("https://hackthefuture.bignited.be");
     cy.get("button[class^='center-button']").click();
-    cy.wait(20000);
     cy.get("div[id='female']").click();
-    cy.wait(1000);
     cy.get("button").should('contain', 'Yes').click();
   });
 })
---
+++ cypress/e2e/util.js
@@ -0,0 +1,25 @@
+export function disableMotion(win) {
+  const injectedStyleEl = win.document.getElementById('__cy_disable_motion__');
+  if (injectedStyleEl) {
+    return;
+  }
+  win.document.head.insertAdjacentHTML(
+    'beforeend',
+    `
+    <style id="__cy_disable_motion__">
+      /* Disable CSS transitions. */
+      *, *::before, *::after { -webkit-transition: none !important; -moz-transition: none !important; -o-transition: none !important; -ms-transition: none !important; transition: none !important; }
+      /* Disable CSS animations. */
+      *, *::before, *::after { -webkit-animation: none !important; -moz-animation: none !important; -o-animation: none !important; -ms-animation: none !important; animation: none !important; }
+    </style>
+  `.trim()
+  );
+}
+
+export function enableMotion(win) {
+  const injectedStyleEl = win.document.getElementById('__cy_disable_motion__');
+  if (!injectedStyleEl) {
+    return;
+  }
+  injectedStyleEl.parentNode.removeChild(injectedStyleEl);
+}
