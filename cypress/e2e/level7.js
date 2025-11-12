79cc9eb5767ec7a94daafddf6f282978a9971ba4
Robin Breugelmans <s148461@ap.be>
2025-11-12 14:18:07 +0100

    something

--- cypress/e2e/level7.js
+++ cypress/e2e/level7.js
@@ -5,16 +5,27 @@ export function level7() {
     let letters = cy.get(`div.draggable-cube[data-letter='${word[i]}']`);
     let targets = cy.get(`div.target-slot[data-letter='${word[i]}']`);
     
-    if (!Array.isArray(letters)) {
-      letters = [letters];
-      targets = [targets];
-    }
+    letters.then(($letters => {
+      for (let j = 0; j < $letters.length; ++j) {
+        const letter = cy.wrap($letters[j]);
+        // letter.trigger("dragstart", { dataTransfer });
+        
+        targets.then(($targets => {
+          const target = cy.wrap($targets[j]);
+          letter.drag(target);
+          // target.trigger("drop", { dataTransfer });
+        }));
+
+        // letter.trigger("dragend", { dataTransfer });
+      }
+    }));
+
     
     const dataTransfer = new DataTransfer;
-    for (let j = 0; j < letters.length;) {
-      // letters[j].trigger("dragstart", { dataTransfer });
-      // targets[j].trigger("drop", { dataTransfer });
-      // letters[j].trigger("dragend", { dataTransfer });
+    for (let j = 0; j < letters.length; ++j) {
+      letters[j].trigger("dragstart", { dataTransfer });
+      targets[j].trigger("drop", { dataTransfer });
+      letters[j].trigger("dragend", { dataTransfer });
     }
   }
 }
