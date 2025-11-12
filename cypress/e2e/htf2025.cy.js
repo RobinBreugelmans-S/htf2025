9459bd48077885b98674dfefba62fef80ec4fb21
SkyPromp <poppe1max@gmail.com>
2025-11-12 10:33:07 +0100

    load

--- cypress/e2e/htf2025.cy.js
+++ cypress/e2e/htf2025.cy.js
@@ -4,10 +4,15 @@ Cypress.on('window:before:load', (cyWindow) => {
   disableMotion(cyWindow);
 });
 
+Cypress.on('window:load', (cyWindow) => {
+  disableMotion(cyWindow);
+});
+
 describe("template spec", () => {
   it("passes", () => {
     cy.visit("https://hackthefuture.bignited.be");
     cy.get("button[class^='center-button']").click();
+    cy.wait(100);
     cy.get("div[id='female']").click();
     cy.get("button").should('contain', 'Yes').click();
   });
