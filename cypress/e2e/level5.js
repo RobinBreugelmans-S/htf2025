e67740b4b81ba0b8e9459edd461823f51e88aae4
SkyPromp <poppe1max@gmail.com>
2025-11-12 12:03:17 +0100

    level 5

--- cypress/e2e/level5.js
+++ cypress/e2e/level5.js
@@ -1,10 +1,14 @@
 export function level5(){
   cy.tick(25000);
+  console.log("level5");
 
-  for (let i = 1; i <= 4; i++){
-    let button = cy.get(`div[class^='square-${i}']`);
-    button.click();
+  for (let i = 0; i < 4; i++){
+    let button = cy.get(`div[id^='square-${i}']`);
+    button.trigger('mouseover', {force: true});
     cy.tick(25000);
+    console.log(button.attr('class'));
+    let crystal = cy.get(`div[id='crystal']`);
+    console.log(crystal);
   }
   cy.tick(25000);
 }
