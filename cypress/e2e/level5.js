0a66fd7811c6d6b90c9f373025bfda560b6fde0f
SkyPromp <poppe1max@gmail.com>
2025-11-12 15:32:03 +0100

    4

--- cypress/e2e/level5.js
+++ cypress/e2e/level5.js
@@ -1,19 +1,37 @@
+// export function level5() {
+//   cy.tick(25000);
+//
+//
+//   for (let i = 0; i < 4; i++){
+//     let button = cy.get(`div[id='square-${i}']`);
+//     let button2 = cy.get(`div[id='square-${i}']`);
+//
+//     button.trigger('mouseover', {force: true});
+//     cy.tick(25000);
+//
+//     button.invoke('attr', 'class').then((classes) => {
+//       if(classes.includes("active")) {
+//         button2.dblclick({force: true});
+//         return;
+//       }
+//     });
+//   }
+//
+//   cy.tick(25000);
+// }
+
 export function level5() {
   cy.tick(25000);
 
-  try{
-    for (let i = 0; i < 4; i++){
-      let button = cy.get(`div[id='square-${i}']`);
-      let button2 = cy.get(`div[id='square-${i}']`);
+  cy.get(`div[id^='square-']`).each((value) => {
+    cy.wrap(value).trigger("mouseover", {force: true});
+    cy.tick(25000);
 
-      button.trigger('mouseover', {force: true});
-      cy.tick(25000);
-
-      button.invoke('attr', 'class').then((classes) => {
-        if(classes.includes("active")) button2.dblclick({force: true});
-      });
-    }
-  } catch(e){}
-
-  cy.tick(25000);
+    cy.wrap(value).invoke('attr', 'class').then((classes) => {
+      if(classes.includes("active")) {
+        cy.wrap(value).dblclick({force: true});
+        return false;
+      }
+    });
+  });
 }
